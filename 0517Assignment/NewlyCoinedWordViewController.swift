b9bef9d38be92ffda15971cbc0a7ed50ef868d46
iyeahh <yboravender@gmail.com>
2024-05-19 13:30:35 +0900

    feat: 스토리보드 - ViewController outlet, action 연결

---
+++ 0517Assignment/NewlyCoinedWordViewController.swift
@@ -0,0 +1,41 @@
+//
+//  NewlyCoinedWordViewController.swift
+//  0517Assignment
+//
+//  Created by Bora Yang on 5/19/24.
+//
+
+import UIKit
+
+class NewlyCoinedWordViewController: UIViewController {
+
+    @IBOutlet var backgroundView: UIView!
+    @IBOutlet var inputTextField: UITextField!
+    @IBOutlet var searchButton: UIButton!
+
+    @IBOutlet var searchWordButton1: UIButton!
+    @IBOutlet var searchWordButton2: UIButton!
+    @IBOutlet var searchWordButton3: UIButton!
+    @IBOutlet var searchWordButton4: UIButton!
+
+    @IBOutlet var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func searchButtonTapped(_ sender: UIButton) {
+    }
+
+    @IBAction func searchWordButtonTapped1(_ sender: UIButton) {
+    }
+    
+    @IBAction func searchWordButtonTapped2(_ sender: UIButton) {
+    }
+    
+    @IBAction func searchWordButtonTapped3(_ sender: UIButton) {
+    }
+    
+    @IBAction func searchWordButtonTapped4(_ sender: UIButton) {
+    }
+}
