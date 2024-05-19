2eb885a544b30ac260630581c3a19ac30912e587
iyeahh <yboravender@gmail.com>
2024-05-19 12:32:47 +0900

    feat: 스토리보드 - Viewcontroller outlet, action 연결

---
+++ 0517Assignment/EmotionDiaryViewController.swift
@@ -0,0 +1,58 @@
+//
+//  EmotionDiaryViewController.swift
+//  0517Assignment
+//
+//  Created by Bora Yang on 5/19/24.
+//
+
+import UIKit
+
+class EmotionDiaryViewController: UIViewController {
+
+    // MARK: - button outlet 연결
+    @IBOutlet var happyButton: UIButton!
+    @IBOutlet var loveButton: UIButton!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var panicButton: UIButton!
+    @IBOutlet var upsetButton: UIButton!
+    @IBOutlet var depressedButton: UIButton!
+    @IBOutlet var boredButton: UIButton!
+    @IBOutlet var sulkingButton: UIButton!
+    @IBOutlet var tearfulButton: UIButton!
+
+    // MARK: - label outlet 연결
+    @IBOutlet var happyLabel: UILabel!
+    @IBOutlet var loveLabel: UILabel!
+    @IBOutlet var likeLabel: UILabel!
+    @IBOutlet var panicLabel: UILabel!
+    @IBOutlet var upsetLabel: UILabel!
+    @IBOutlet var depressedLabel: UILabel!
+    @IBOutlet var boredLabel: UILabel!
+    @IBOutlet var sulkingLabel: UILabel!
+    @IBOutlet var tearfulLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - button action 연결
+    @IBAction func happyButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func loveButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func likeButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func panicButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func upsetButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func depressedButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func boredButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func sulkingButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func tearfulButtonTapped(_ sender: UIButton) {
+    }
+}
