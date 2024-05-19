e07b6a7d2dddf5cedd049960afbaf33310487892
iyeahh <yboravender@gmail.com>
2024-05-19 13:12:52 +0900

    feat: 버튼 터치 시 레이블 텍스트 변경되는 로직 구현

--- 0517Assignment/EmotionDiaryViewController.swift
+++ 0517Assignment/EmotionDiaryViewController.swift
@@ -31,6 +31,16 @@ class EmotionDiaryViewController: UIViewController {
     @IBOutlet var sulkingLabel: UILabel!
     @IBOutlet var tearfulLabel: UILabel!
 
+    var happyNumber = 1
+    var loveNumber = 1
+    var likeNumber = 1
+    var panicNumber = 1
+    var upsetNumber = 1
+    var depressedNumber = 1
+    var boredNumber = 1
+    var sulkingNumber = 1
+    var tearfulNumber = 1
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.8609215617, green: 0.8262683749, blue: 0.7879908681, alpha: 1)
@@ -50,51 +60,69 @@ class EmotionDiaryViewController: UIViewController {
     }
 
     func setupLableText() {
-        happyLabel.text = "행복해 1"
+        happyLabel.text = "행복해 \(happyNumber)"
         happyLabel.textAlignment = .center
 
-        loveLabel.text = "사랑해 1"
+        loveLabel.text = "사랑해 \(loveNumber)"
         loveLabel.textAlignment = .center
 
-        likeLabel.text = "좋아해 1"
+        likeLabel.text = "좋아해 \(likeNumber)"
         likeLabel.textAlignment = .center
 
-        panicLabel.text = "당황해 1"
+        panicLabel.text = "당황해 \(panicNumber)"
         panicLabel.textAlignment = .center
 
-        upsetLabel.text = "속상해 1"
+        upsetLabel.text = "속상해 \(upsetNumber)"
         upsetLabel.textAlignment = .center
 
-        depressedLabel.text = "우울해 1"
+        depressedLabel.text = "우울해 \(depressedNumber)"
         depressedLabel.textAlignment = .center
 
-        boredLabel.text = "심심해 1"
+        boredLabel.text = "심심해 \(boredNumber)"
         boredLabel.textAlignment = .center
 
-        sulkingLabel.text = "삐졌어 1"
+        sulkingLabel.text = "삐졌어 \(sulkingNumber)"
         sulkingLabel.textAlignment = .center
 
-        tearfulLabel.text = "눈물나 1"
+        tearfulLabel.text = "눈물나 \(tearfulNumber)"
         tearfulLabel.textAlignment = .center
     }
 
     // MARK: - button action 연결
     @IBAction func happyButtonTapped(_ sender: UIButton) {
+        happyNumber += 1
+        happyLabel.text = "행복해 \(happyNumber)"
     }
     @IBAction func loveButtonTapped(_ sender: UIButton) {
+        loveNumber += 1
+        loveLabel.text = "사랑해 \(loveNumber)"
     }
     @IBAction func likeButtonTapped(_ sender: UIButton) {
+        likeNumber += 1
+        likeLabel.text = "좋아해 \(likeNumber)"
     }
     @IBAction func panicButtonTapped(_ sender: UIButton) {
+        panicNumber += 1
+        panicLabel.text = "당황해 \(panicNumber)"
     }
     @IBAction func upsetButtonTapped(_ sender: UIButton) {
+        upsetNumber += 1
+        upsetLabel.text = "속상해 \(upsetNumber)"
     }
     @IBAction func depressedButtonTapped(_ sender: UIButton) {
+        depressedNumber += 1
+        depressedLabel.text = "우울해 \(depressedNumber)"
     }
     @IBAction func boredButtonTapped(_ sender: UIButton) {
+        boredNumber += 1
+        boredLabel.text = "심심해 \(boredNumber)"
     }
     @IBAction func sulkingButtonTapped(_ sender: UIButton) {
+        sulkingNumber += 1
+        sulkingLabel.text = "삐졌어 \(sulkingNumber)"
     }
     @IBAction func tearfulButtonTapped(_ sender: UIButton) {
+        tearfulNumber += 1
+        tearfulLabel.text = "눈물나 \(tearfulNumber)"
     }
 }
