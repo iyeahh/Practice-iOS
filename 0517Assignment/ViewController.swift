01e0e56f29ee847abccbdb3ddca8962cd946c31a
iyeahh <yboravender@gmail.com>
2024-05-19 12:04:55 +0900

    Initial Commit

---
+++ 0517Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  0517Assignment
+//
+//  Created by Bora Yang on 5/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
