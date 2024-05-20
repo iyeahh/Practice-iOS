c93eaf8e82f85eff919051643accc75fbfbbabb5
iyeahh <yboravender@gmail.com>
2024-05-20 21:00:25 +0900

    refactor: HomeViewController 리팩토링

--- 0514Practice/HomeViewController.swift
+++ 0514Practice/HomeViewController.swift
@@ -8,27 +8,26 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
     @IBOutlet var moviePosterImageView: UIImageView!
     @IBOutlet var playButton: UIButton!
     @IBOutlet var plusButton: UIButton!
+
     @IBOutlet var randomImageView1: UIImageView!
     @IBOutlet var randomImageView2: UIImageView!
     @IBOutlet var randomImageView3: UIImageView!
 
+    @IBOutlet var imageViewOutletCollection: [UIImageView]!
+
     @IBOutlet var label1: UILabel!
     @IBOutlet var label2: UILabel!
-
     @IBOutlet var label3: UILabel!
     @IBOutlet var label4: UILabel!
-    
     @IBOutlet var label5: UILabel!
     @IBOutlet var label6: UILabel!
 
     @IBOutlet var badge1: UIImageView!
     @IBOutlet var badge2: UIImageView!
     @IBOutlet var badge3: UIImageView!
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,24 +35,32 @@ class HomeViewController: UIViewController {
         view.backgroundColor = .black
         navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
 
-        moviePosterImageView.contentMode = .scaleAspectFill
-        moviePosterImageView.backgroundColor = .red
-        moviePosterImageView.layer.cornerRadius = 10
+        imageViewOutletCollection.forEach { imageView in
+            imageView.contentMode = .scaleAspectFill
+            imageView.backgroundColor = .red
+            imageView.layer.cornerRadius = 10
+        }
 
-        randomImageView1.contentMode = .scaleAspectFill
-        randomImageView1.backgroundColor = .red
-        randomImageView1.layer.cornerRadius = 10
-
-        randomImageView2.contentMode = .scaleAspectFill
-        randomImageView2.backgroundColor = .red
-        randomImageView2.layer.cornerRadius = 10
-
-        randomImageView3.contentMode = .scaleAspectFill
-        randomImageView3.backgroundColor = .red
-        randomImageView3.layer.cornerRadius = 10
+        setupMoviePosterImageViewUI()
+        setupPlayButtonUI()
+        setupPlusButtonUI()
 
         randomPosterImage()
 
+        randomBanner1()
+        randomBanner2()
+        randomBanner3()
+
+        randomBadge()
+    }
+
+    func setupMoviePosterImageViewUI() {
+        moviePosterImageView.contentMode = .scaleAspectFill
+        moviePosterImageView.backgroundColor = .red
+        moviePosterImageView.layer.cornerRadius = 10
+    }
+
+    func setupPlayButtonUI() {
         playButton.backgroundColor = .white
         playButton.layer.masksToBounds = true
         playButton.layer.cornerRadius = 5
@@ -62,7 +69,9 @@ class HomeViewController: UIViewController {
         playButton.tintColor = .black
         playButton.setTitle("재생", for: .normal)
         playButton.setTitleColor(.black, for: .normal)
+    }
 
+    func setupPlusButtonUI() {
         plusButton.backgroundColor = .darkGray
         plusButton.layer.masksToBounds = true
         plusButton.layer.cornerRadius = 5
@@ -71,121 +80,77 @@ class HomeViewController: UIViewController {
         plusButton.tintColor = .white
         plusButton.setTitle("내가 찜한 리스트", for: .normal)
         plusButton.setTitleColor(.white, for: .normal)
+    }
 
-        badge1.image = UIImage(named: "top10 badge")
-        badge2.image = UIImage(named: "top10 badge")
-        badge3.image = UIImage(named: "top10 badge")
+    func newEpisode(label1: UILabel, label2: UILabel) {
+        label1.isHidden = false
+        label2.isHidden = false
 
-        randomBanner1()
-        randomBanner2()
-        randomBanner3()
+        label1.backgroundColor = .red
+        label1.text = "새로운 에피소드"
+        label1.textColor = .white
+        label1.layer.masksToBounds = true
+        label1.layer.cornerRadius = 5
+        label1.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        label1.textAlignment = .center
+
+        label2.backgroundColor = .white
+        label2.text = "지금 시청하기"
+        label2.textColor = .black
+        label2.layer.masksToBounds = true
+        label2.layer.cornerRadius = 5
+        label2.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        label2.textAlignment = .center
+    }
+
+    func newSeries(label1: UILabel, label2: UILabel) {
+        label1.isHidden = true
+        label2.isHidden = false
+
+        label2.backgroundColor = .red
+        label2.text = "새로운 시리즈"
+        label2.textColor = .white
+        label2.layer.masksToBounds = true
+        label2.layer.cornerRadius = 5
+        label2.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        label2.textAlignment = .center
+    }
+
+    func noneBanner(label1: UILabel, label2: UILabel) {
+        label1.isHidden = true
+        label2.isHidden = true
     }
 
     func randomBanner1() {
         let num = Int.random(in: 0...5)
         if num == 1 {
-            label1.isHidden = false
-            label2.isHidden = false
-            label1.backgroundColor = .red
-            label1.text = "새로운 에피소드"
-            label1.textColor = .white
-            label1.layer.masksToBounds = true
-            label1.layer.cornerRadius = 5
-            label1.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
-            label1.textAlignment = .center
-
-            label2.backgroundColor = .white
-            label2.text = "지금 시청하기"
-            label2.textColor = .black
-            label2.layer.masksToBounds = true
-            label2.layer.cornerRadius = 5
-            label2.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
-            label2.textAlignment = .center
+            newEpisode(label1: label1, label2: label2)
         } else if num == 2 {
-            label1.isHidden = true
-            label2.isHidden = false
-            label2.backgroundColor = .red
-            label2.text = "새로운 시리즈"
-            label2.textColor = .white
-            label2.layer.masksToBounds = true
-            label2.layer.cornerRadius = 5
-            label2.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
-            label2.textAlignment = .center
+            newSeries(label1: label1, label2: label2)
         } else {
-            label1.isHidden = true
-            label2.isHidden = true
+            noneBanner(label1: label1, label2: label2)
         }
     }
 
     func randomBanner2() {
         let num = Int.random(in: 0...5)
         if num == 1 {
-            label3.isHidden = false
-            label4.isHidden = false
-            label3.backgroundColor = .red
-            label3.text = "새로운 에피소드"
-            label3.textColor = .white
-            label3.layer.masksToBounds = true
-            label3.layer.cornerRadius = 5
-            label3.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
-            label3.textAlignment = .center
-
-            label4.backgroundColor = .white
-            label4.text = "지금 시청하기"
-            label4.textColor = .black
-            label4.layer.masksToBounds = true
-            label4.layer.cornerRadius = 5
-            label4.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
-            label4.textAlignment = .center
+            newEpisode(label1: label3, label2: label4)
         } else if num == 2 {
-            label3.isHidden = true
-            label4.isHidden = false
-            label4.backgroundColor = .red
-            label4.text = "새로운 시리즈"
-            label4.textColor = .white
-            label4.layer.masksToBounds = true
-            label4.layer.cornerRadius = 5
-            label4.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
-            label4.textAlignment = .center
+            newSeries(label1: label3, label2: label4)
         } else {
-            label3.isHidden = true
-            label4.isHidden = true
+            noneBanner(label1: label3, label2: label4)
         }
     }
 
     func randomBanner3() {
         let num = Int.random(in: 0...5)
         if num == 1 {
-            label5.isHidden = false
-            label6.isHidden = false
-            label5.backgroundColor = .red
-            label5.text = "새로운 에피소드"
-            label5.textColor = .white
-            label5.layer.masksToBounds = true
-            label5.layer.cornerRadius = 5
-            label5.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
-            label5.textAlignment = .center
-
-            label6.backgroundColor = .white
-            label6.text = "지금 시청하기"
-            label6.textColor = .black
-            label6.layer.masksToBounds = true
-            label6.layer.cornerRadius = 5
-            label6.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
-            label6.textAlignment = .center
+            newEpisode(label1: label5, label2: label6)
         } else if num == 2 {
-            label5.isHidden = true
-            label6.isHidden = false
-            label6.backgroundColor = .red
-            label6.text = "새로운 시리즈"
-            label6.textColor = .white
-            label6.layer.masksToBounds = true
-            label6.layer.cornerRadius = 5
-            label6.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
-            label6.textAlignment = .center
+            newSeries(label1: label5, label2: label6)
         } else {
-            label5.isHidden = true
-            label6.isHidden = true
+            noneBanner(label1: label5, label2: label6)
         }
     }
 
@@ -209,6 +174,29 @@ class HomeViewController: UIViewController {
         movies.remove(at: num3)
     }
 
+    func randomBadge() {
+        var num1 = Int.random(in: 0...1)
+        var num2 = Int.random(in: 0...1)
+        var num3 = Int.random(in: 0...1)
+
+        if num1 == 0 {
+            badge1.image = UIImage(named: "top10 badge")
+        } else {
+            badge1.isHidden = true
+        }
+
+        if num1 == 0 {
+            badge2.image = UIImage(named: "top10 badge")
+        } else {
+            badge2.isHidden = true
+        }
+
+        if num1 == 0 {
+            badge3.image = UIImage(named: "top10 badge")
+        } else {
+            badge3.isHidden = true
+        }
+    }
 
     @IBAction func playButtonTapped(_ sender: UIButton) {
         randomPosterImage()
@@ -216,6 +204,7 @@ class HomeViewController: UIViewController {
         randomBanner1()
         randomBanner2()
         randomBanner3()
+
+        randomBadge()
     }
-    
 }
