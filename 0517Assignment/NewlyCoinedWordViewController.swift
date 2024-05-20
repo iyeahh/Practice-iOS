da09ab0741134fe641aeb409f8c70498ae6be5ed
iyeahh <yboravender@gmail.com>
2024-05-20 17:26:18 +0900

    refactor: OutletCollection, Any 활용하여 리팩토링

--- 0517Assignment/NewlyCoinedWordViewController.swift
+++ 0517Assignment/NewlyCoinedWordViewController.swift
@@ -14,12 +14,7 @@ class NewlyCoinedWordViewController: UIViewController, UIGestureRecognizerDelega
     @IBOutlet var backgroundView: UIView!
     @IBOutlet var inputTextField: UITextField!
     @IBOutlet var searchButton: UIButton!
-
-    @IBOutlet var searchWordButton1: UIButton!
-    @IBOutlet var searchWordButton2: UIButton!
-    @IBOutlet var searchWordButton3: UIButton!
-    @IBOutlet var searchWordButton4: UIButton!
-
+    @IBOutlet var buttonCollection: [UIButton]!
     @IBOutlet var resultLabel: UILabel!
     
     override func viewDidLoad() {
@@ -48,15 +43,14 @@ class NewlyCoinedWordViewController: UIViewController, UIGestureRecognizerDelega
     func setupSearchWordButtonUI() {
         setSearchWordButtonRandomTitle()
 
-        let buttons = [searchWordButton1, searchWordButton2, searchWordButton3, searchWordButton4]
-
-        buttons.forEach { button in
-            button?.configuration?.buttonSize = .small
-            button?.layer.masksToBounds = true
-            button?.layer.cornerRadius = 10
-            button?.layer.borderWidth = 1
-            button?.layer.borderColor = UIColor.black.cgColor
-            button?.tintColor = .black
+        // OuletCollection 활용
+        buttonCollection.forEach { button in
+            button.configuration?.buttonSize = .small
+            button.layer.masksToBounds = true
+            button.layer.cornerRadius = 10
+            button.layer.borderWidth = 1
+            button.layer.borderColor = UIColor.black.cgColor
+            button.tintColor = .black
         }
     }
 
@@ -88,47 +82,23 @@ class NewlyCoinedWordViewController: UIViewController, UIGestureRecognizerDelega
     func setSearchWordButtonRandomTitle() {
         var newlyCoinedWord = [("원영적 사고", "역시 행운의 여신은 나의 편이야\n럭키비키잖아 >.~"), ("중꺾그마", "중요한 건 꺾여도 그냥 하는 마음 🥹"), ("KIJUL", "너무 재밌어서 기절하겠다 ^_^"), ("기나죄", "기분 나빴다면 죄송합니다 ㅎ.ㅎ"), ("지팔지꼰", "자기 팔자 자기가 꼰다 😊")]
 
-        let firstButtonName = newlyCoinedWord.randomElement()?.0
-        searchWordButton1.setTitle(firstButtonName, for: .normal)
-        let num1 = newlyCoinedWord.firstIndex { (keyword, _) in
-            keyword == firstButtonName
+        buttonCollection.forEach { button in
+            let randomWord = newlyCoinedWord.randomElement()?.0
+            button.setTitle(randomWord, for: .normal)
+            let index = newlyCoinedWord.firstIndex { (keyword, _) in
+                keyword == randomWord
+            }
+            newlyCoinedWord.remove(at: index!)
         }
-        newlyCoinedWord.remove(at: num1!)
-
-        let secondButtonName = newlyCoinedWord.randomElement()?.0
-        searchWordButton2.setTitle(secondButtonName, for: .normal)
-        let num2 = newlyCoinedWord.firstIndex { (keyword, _) in
-            keyword == secondButtonName
-        }
-        newlyCoinedWord.remove(at: num2!)
-
-        let thirdButtonName = newlyCoinedWord.randomElement()?.0
-        searchWordButton3.setTitle(thirdButtonName, for: .normal)
-        let num3 = newlyCoinedWord.firstIndex { (keyword, _) in
-            keyword == thirdButtonName
-        }
-        newlyCoinedWord.remove(at: num3!)
-
-        let fourthButtonName = newlyCoinedWord.randomElement()?.0
-        searchWordButton4.setTitle(fourthButtonName, for: .normal)
-        let num4 = newlyCoinedWord.firstIndex { (keyword, _) in
-            keyword == fourthButtonName
-        }
-        newlyCoinedWord.remove(at: num4!)
-
     }
 
-    @IBAction func searchButtonTapped(_ sender: UIButton) {
+    // 중복되는 로직 Any로 구현
+    @IBAction func searchWordTapped(_ sender: Any) {
         searchWord()
         setSearchWordButtonRandomTitle()
     }
 
-    @IBAction func searchTextFieldTapped(_ sender: UITextField) {
-        searchWord()
-        setSearchWordButtonRandomTitle()
-    }
-    
-    @IBAction func searchWordButtonTapped1(_ sender: UIButton) {
+    @IBAction func searchWordButtonTapped(_ sender: UIButton) {
         inputTextField.text = sender.currentTitle
 
         let index = newlyCoinedWord.firstIndex { (keyword, _) in
