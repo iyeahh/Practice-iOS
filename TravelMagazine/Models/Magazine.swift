4b6ca55aedb74dc96e5e4cf66244ad977c32c72d
iyeahh <yboravender@gmail.com>
2024-05-25 12:17:01 +0900

    feat: Model 추가, MVC 패턴으로 폴더링

---
+++ TravelMagazine/Models/Magazine.swift
@@ -0,0 +1,16 @@
+//
+//  Magazine.swift
+//  TravelMagazine
+//
+//  Created by Bora Yang on 5/25/24.
+//
+
+import Foundation
+
+struct Magazine {
+    var title: String
+    var subtitle: String
+    var photo_image: String
+    var date: String
+    var link: String
+}
