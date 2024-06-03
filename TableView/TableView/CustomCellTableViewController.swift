3544732ed1cde9902dc5da6219771786517d6eb9
iyeahh <yboravender@gmail.com>
2024-06-03 14:58:12 +0900

    refactor: 기존 과제 열거형을 활용한 리팩토링

--- TableView/TableView/CustomCellTableViewController.swift
+++ TableView/TableView/CustomCellTableViewController.swift
@@ -7,29 +7,44 @@
 
 import UIKit
 
+enum HeaderView: String, CaseIterable {
+    case all = "전체 설정"
+    case personal = "개인 설정"
+    case etc = "기타"
+
+    var subOptions: [String] {
+        switch self {
+        case .all:
+            return ["공지사항", "실험실", "버전 정보"]
+        case .personal:
+            return ["개인/보안", "알림", "채팅", "멀티프로필"]
+        case .etc:
+            return ["고객센터/도움말"]
+        }
+    }
+}
+
 class CustomCellTableViewController: UITableViewController {
-    let headerString = ["전체 설정", "개인 설정", "기타"]
-    let value = [["공지사항", "실험실", "버전 정보"], ["개인/보안", "알림", "채팅", "멀티프로필"], ["고객센터/도움말"]]
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return headerString[section]
+        return HeaderView.allCases[section].rawValue
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return value.count
+        return HeaderView.allCases.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return value[section].count
+        return HeaderView.allCases[section].subOptions.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath)
-        cell.textLabel?.text = value[indexPath.section][indexPath.row]
+        cell.textLabel?.text = HeaderView.allCases[indexPath.section].subOptions[indexPath.row]
         cell.textLabel?.font = UIFont.systemFont(ofSize: 13)
         return cell
     }
