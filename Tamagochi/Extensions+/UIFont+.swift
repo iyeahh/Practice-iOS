8f7bc29658119d8b8770417ab0a98653cec7f9b6
iyeahh <yboravender@gmail.com>
2024-06-07 12:11:14 +0900

    feat: UIColor, Font, Image extension 구현

---
+++ Tamagochi/Extensions+/UIFont+.swift
@@ -0,0 +1,14 @@
+//
+//  UIFont+.swift
+//  Tamagochi
+//
+//  Created by Bora Yang on 6/7/24.
+//
+
+import UIKit
+
+extension UIFont {
+    static let largeFont = UIFont.systemFont(ofSize: 15)
+    static let mediumFont = UIFont.systemFont(ofSize: 14)
+    static let smallFont = UIFont.systemFont(ofSize: 13)
+}
---
+++ Tamagochi/Extensions+/UIImage+.swift
@@ -0,0 +1,17 @@
+//
+//  UIImage+.swift
+//  Tamagochi
+//
+//  Created by Bora Yang on 6/7/24.
+//
+
+import UIKit
+
+extension UIImage {
+    static let rice = UIImage(systemName: "drop.circle")
+    static let water = UIImage(systemName: "leaf.circle")
+
+    static let pencil = UIImage(systemName: "pencil")
+    static let moon = UIImage(systemName: "moon.fill")
+    static let reset = UIImage(systemName: "arrow.clockwise")
+}
