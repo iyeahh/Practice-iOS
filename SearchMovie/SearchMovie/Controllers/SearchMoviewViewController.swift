8526be6d16ee326fb93b9f76cd92f3524f6bd7ff
iyeahh <yboravender@gmail.com>
2024-06-11 16:52:35 +0900

    conf: 초기 설정

---
+++ SearchMovie/SearchMovie/Controllers/SearchMoviewViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchMoviewViewController.swift
+//  SearchMovie
+//
+//  Created by Bora Yang on 6/11/24.
+//
+
+import UIKit
+
+class SearchMoviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
