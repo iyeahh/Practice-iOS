9b5538984107b6ad89f724a518d57d7e8e9b10f1
iyeahh <yboravender@gmail.com>
2024-06-19 15:14:22 +0900

    feat: CoreLocation을 활용하여 현재 위치 annotation 추가

--- TravelMagazine/TravelMagazine/RestaurantList/Controllers/MapViewController.swift
+++ TravelMagazine/TravelMagazine/RestaurantList/Controllers/MapViewController.swift
@@ -6,18 +6,21 @@
 //
 
 import UIKit
+import CoreLocation
 import MapKit
 
 class MapViewController: UIViewController {
     var viewModel: RestaurantViewModel?
+    let locationManager = CLLocationManager()
 
     @IBOutlet var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationItem()
-        setupAllAnnotation()
+//        setupAllAnnotation()
         setupCenterLocation()
+        locationManager.delegate = self
     }
 
     private func setupNavigationItem() {
@@ -31,13 +34,13 @@ class MapViewController: UIViewController {
         navigationItem.rightBarButtonItem = filterButton
     }
 
-    private func setupAllAnnotation() {
-        guard let restaurantList = viewModel?.restaurantListData else {
-            return
-        }
-
-        addAnnotation(retaurants: restaurantList)
-    }
+//    private func setupAllAnnotation() {
+//        guard let restaurantList = viewModel?.restaurantListData else {
+//            return
+//        }
+//
+//        addAnnotation(retaurants: restaurantList)
+//    }
 
     private func setupCenterLocation() {
         guard let restaurant = viewModel?.restaurantListData.randomElement() else {
@@ -60,16 +63,16 @@ class MapViewController: UIViewController {
         )
     }
 
-    private func filterRestaurant(category: Category) {
-        if category == .all {
-            setupAllAnnotation()
-        } else {
-            if let filteredList = viewModel?.restaurantListData.filter({ restaurant in restaurant.category == category
-            }) {
-                addAnnotation(retaurants: filteredList)
-            }
-        }
-    }
+//    private func filterRestaurant(category: Category) {
+//        if category == .all {
+//            setupAllAnnotation()
+//        } else {
+//            if let filteredList = viewModel?.restaurantListData.filter({ restaurant in restaurant.category == category
+//            }) {
+//                addAnnotation(retaurants: filteredList)
+//            }
+//        }
+//    }
 
     @objc private func filterButtonTapped() {
         let actionSheet = UIAlertController(
@@ -80,23 +83,26 @@ class MapViewController: UIViewController {
         let all = UIAlertAction(
             title: "전체보기",
             style: .default,
-            handler: { _ in
-                self.filterRestaurant(category: .all)
-            }
+//            handler: { _ in
+//                self.filterRestaurant(category: .all)
+//            }
+            handler: nil
         )
 
         let koreanFood = UIAlertAction(
             title: "한식",
             style: .default,
-            handler: { _ in self.filterRestaurant(category: .korean)
-            }
+//            handler: { _ in self.filterRestaurant(category: .korean)
+//            }
+            handler: nil
         )
 
         let chineseFood = UIAlertAction(
             title: "중식",
             style: .default,
-            handler: { _ in self.filterRestaurant(category: .chinese)
-            }
+//            handler: { _ in self.filterRestaurant(category: .chinese)
+//            }
+            handler: nil
         )
 
         let cancel = UIAlertAction(
@@ -112,26 +118,68 @@ class MapViewController: UIViewController {
         present(actionSheet, animated: true)
     }
 
-    private func makeAnnotations(restaurant: RestaurantInfo) -> MKAnnotation{
+    private func makeAnnotations(lat: CLLocationDegrees, lon: CLLocationDegrees) -> MKAnnotation{
         let location = CLLocationCoordinate2D(
-            latitude: restaurant.latitude,
-            longitude: restaurant.longitude
+            latitude: lat, longitude: lon
         )
 
         let annotation = MKPointAnnotation()
         annotation.coordinate = location
-        annotation.title = restaurant.name
+        annotation.title = "현위치"
 
         return annotation
     }
 
-    private func addAnnotation(retaurants: [RestaurantInfo]) {
+    private func addAnnotation(lat: CLLocationDegrees, lon: CLLocationDegrees) {
         mapView.removeAnnotations(mapView.annotations)
 
-        let annotations = retaurants.map({ retaurant in
-            makeAnnotations(restaurant: retaurant)
-        })
+        let annotation = makeAnnotations(lat: lat, lon: lon)
 
-        mapView.addAnnotations(annotations)
+        mapView.addAnnotation(annotation)
+    }
+}
+
+extension MapViewController {
+    func checkDeviceLocationAutorization() {
+        if CLLocationManager.locationServicesEnabled() {
+            checkCurrentLocationAuthorization()
+        } else {
+            print("위치 서비스 꺼져 있음")
+        }
+    }
+
+    func checkCurrentLocationAuthorization() {
+        var status: CLAuthorizationStatus
+
+        if #available(iOS 14.0, *) {
+            status = locationManager.authorizationStatus
+        } else {
+            status = CLLocationManager.authorizationStatus()
+        }
+
+        switch status {
+        case .notDetermined:
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+        case .denied:
+            print("iOS 설정 창으로 이동")
+        case .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        default:
+            print(status)
+        }
+    }
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let coordinate = locations.last?.coordinate {
+            addAnnotation(lat: coordinate.latitude, lon: coordinate.longitude)
+        }
+        locationManager.startUpdatingLocation()
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkDeviceLocationAutorization()
     }
 }
